6a52251770b2cb8f8697c1ce5a34a6dd7fe420dc
Shayne Boyer <spboyer@live.com>
2016-11-02 11:45:49 -0400

    init

---
+++ src/dotnetapp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message = "Dotnet-bot: Welcome to using .NET Core!";
+
+            if (args.Length > 0)
+            {
+                message = String.Join(" ", args);
+            }
+
+            var robot  = new dotnetbot.maker();
+
+            Console.WriteLine(robot.GetBot(message));
+            Console.WriteLine();
+            Console.WriteLine($"Time: {DateTime.Now.ToString()}");
+            Console.WriteLine();
+            Console.WriteLine($"OS: {RuntimeInformation.OSDescription}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ test/dotnetapp.Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace Tests
+{
+    public class BotTest
+    {
+        [Fact]
+        public void Contains_Message() 
+        {
+            var m = new dotnetbot.maker();
+            var msg = "Testing Dotnet-bot";
+
+            Assert.True(m.GetBot(msg).Contains(msg));
+        }
+    }
+}
